281f5c75dd20a30c5ca92ad16f0748657fe1f791
Josef Gottlander <josef.gottlander@gmail.com>
2023-05-29 22:30:06 +0200

    added home, about and report pages, header, nav, footer and some style

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,8 @@ Encore
      */
     .addEntry('app', './assets/app.js')
 
+    .addStyleEntry('global', './node_modules/normalize.css/normalize.css')
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
